dafc065da1681a4724c0cdda47e74a706161969f
phool.jha <phool.jha@accelya.com>
2022-01-27 12:26:46 +0530

    Added logger

--- src/test/java/com/pitest/pitestexample/base/MyClassTest.java
+++ src/test/java/com/pitest/pitestexample/base/MyClassTest.java
@@ -2,12 +2,15 @@ package com.pitest.pitestexample.base;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.logging.Logger;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.logging.LoggerFactory;
 
 class MyClassTest {
 
     private MyClass myClass;
+    private Logger logger = (Logger) LoggerFactory.getLogger(MyClassTest.class);
 
     @BeforeEach
     void setUp() {
@@ -17,11 +20,13 @@ class MyClassTest {
     @Test
     public void ShouldPassWhenValueIsOne() {
         myClass.myFunction(1);
+        logger.info("Executing ShouldPassWhenValueIsOne");
     }
 
     @Test
     public void ShouldNotPassWhenValueIsTen() {
         myClass.myFunction(10);
+        logger.info("Executing ShouldNotPassWhenValueIsTen");
     }
 
 //    @Test
